39d3e4f9bbdab26a618a4bc281b0102044a0e4c7
Delight Sheriff <delightsheriff@gmail.com>
2024-11-12 08:41:04 +0100

    propertyId page

--- src/app/property/[propertyId]/page.tsx
+++ src/app/property/[propertyId]/page.tsx
@@ -1,26 +1,59 @@
+// import PropertyDetails from "@/components/Property/PropertyDetails";
+// import { fetchProperties, getProperty } from "@/lib/property";
+// import { unstable_noStore as noStore } from "next/cache";
+// import { notFound } from "next/navigation";
+// import React from "react";
+
+// export async function generateStaticParams() {
+//   noStore(); // Disable Next.js caching for this component
+//   const properties = await fetchProperties();
+
+//   const ids = properties.map((property) => ({
+//     PropertyId: String(property._id),
+//   }));
+
+//   return ids;
+// }
+
+// interface Pageprops {
+//   params: { propertyId: string };
+// }
+
+// const Page: React.FC<Pageprops> = async ({ params }) => {
+//   noStore(); // Disable Next.js caching for this component
+//   const property = await getProperty(params.propertyId);
+//   if (!property) notFound();
+//   return <PropertyDetails property={property} />;
+// };
+
+// export default Page;
+
 import PropertyDetails from "@/components/Property/PropertyDetails";
 import { fetchProperties, getProperty } from "@/lib/property";
 import { unstable_noStore as noStore } from "next/cache";
 import { notFound } from "next/navigation";
 import React from "react";
 
+// Type for the page props
+type PageProps = {
+  params: {
+    propertyId: string;
+  };
+};
+
+// Define the generateStaticParams function
 export async function generateStaticParams() {
-  noStore(); // Disable Next.js caching for this component
+  noStore();
   const properties = await fetchProperties();
 
-  const ids = properties.map((property) => ({
-    PropertyId: String(property._id),
+  return properties.map((property) => ({
+    propertyId: String(property._id),
   }));
-
-  return ids;
 }
 
-interface Pageprops {
-  params: { propertyId: string };
-}
-
-const Page: React.FC<Pageprops> = async ({ params }) => {
-  noStore(); // Disable Next.js caching for this component
+// Page component with correct typing
+const Page = async ({ params }: PageProps) => {
+  noStore();
   const property = await getProperty(params.propertyId);
   if (!property) notFound();
   return <PropertyDetails property={property} />;
