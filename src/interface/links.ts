3a5ba17ba08b616b6d6649cb4b6b5c9038125aea
Delight Sheriff <delightsheriff@gmail.com>
2024-11-12 08:45:59 +0100

    header links

--- src/interface/links.ts
+++ src/interface/links.ts
@@ -3,7 +3,7 @@ export type NavItem = readonly [string, string];
 export const navItems: readonly NavItem[] = [
   ["Home", "/"],
   ["About", "/about"],
-  ["Properties", "/properties"],
+  ["Properties", "/property"],
   ["Blog", "/blog"],
   ["Agents", "/agents"],
   ["Contact", "/contact"],
